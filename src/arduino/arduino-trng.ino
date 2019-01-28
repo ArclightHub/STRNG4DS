c47daf53a2b1ab44a84989a200801dc978ed4160
Puppetmaster <gamingzoneservers@gmail.com>
2019-01-27 19:44:25 -0500

    Updated debounce catcher

--- src/arduino/arduino-trng.ino
+++ src/arduino/arduino-trng.ino
@@ -1,9 +1,10 @@
 int count = 0;
 int prev = 0;
+int start = 0;
+int delta;
 const byte interruptPin = 21;
 char bufferOutput[4];
 unsigned int a,b,c,d;
-int start = 0;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -19,7 +20,12 @@ void loop() {
 
 void blink() {
   count = (int)((micros()/8)%24576);
-  if(abs(count-prev) < 64 || start == 0) {
+  delta = abs(count-prev);
+  if(
+    delta < 8
+    || delta > 24568
+    || start == 0
+  ) {
     //Debounce
     prev = count;
     return;
