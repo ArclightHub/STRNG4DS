bfcba3fafe4b11c5f650b54fa8b29bafe81666a9
Puppet <gamingzoneservers@gmail.com>
2020-08-21 12:11:50 +1000

    Updated arduino code and defaults for uno.

--- src/arduino/arduino-trng.ino
+++ src/arduino/arduino-trng.ino
@@ -1,17 +1,27 @@
+// See micros reference page for multiplier: https://www.arduino.cc/reference/en/language/functions/time/micros/
+int boardTimingPrecision = 8;
+
+// Debounce time period, you may need to make this smaller if your beta source is very strong, however it is not recommended.
+// A larger number improves the quality of your random at the cost of lowering the bit stream rate.
+int debounceTime = 386;
+
+// Vars
+int deboundeCeiling = 24576 - debounceTime;
 int count = 0;
 int prev = 0;
 int start = 0;
 int delta;
-const byte interruptPin = 21;
+const byte interruptPin = 2; // Trigger pin, change as needed.
 char bufferOutput[4];
 unsigned int a,b,c,d;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  pinMode(interruptPin, INPUT_PULLUP);
+  //pinMode(interruptPin, INPUT_PULLUP); // Some counter boards need pullup, uncomment if yours pulls to ground as trigger.
+  pinMode(interruptPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
-  delay(2000);
+  delay(500);
   start = 1;
 }
 
@@ -19,20 +29,19 @@ void loop() {
 }
 
 void blink() {
-  count = (int)((micros()/8)%24576);
+  count = (int)((micros()/boardTimingPrecision)%24576);
   delta = abs(count-prev);
   if(
-    delta < 64
-    || delta > 24512
+    delta < debounceTime
+    || delta > deboundeCeiling
     || start == 0
   ) {
     //Debounce
     prev = count;
     return;
   }
-  c = count%256;
+  c,d = count%256;
   a = c>>4;
-  d = count%256;
   b = (d&0x0f);
   sprintf(bufferOutput, "|%x|%x",a,b);
   if (Serial.availableForWrite()) {
